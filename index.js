cf9b5b64d4027e63cccd42fd98655326b5e2ee7a
younes makhchan <younessmakhchane@gmail.com>
2024-04-27 19:17:12 +0100

    adding imports

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+
+// Importing required modules
+
+
 // Importing required modules
 const express = require('express');
+const logger = require('./config/logger');
+const routes = require('./app/route');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Creating an instance of express app
 const app = express();
 
+app.use(express.static('./public'));
 // Define a route
 app.get('/', (req, res) => {
   res.send('Hello, world!');
